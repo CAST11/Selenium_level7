893e341c9811f3da8af5feb4fd03a736c7ccc434
CAST <dona_dea@hotmail.com>
2025-12-12 14:26:23 -0600

    Headless configuration addeed to run Jenkins

--- conftest.py
+++ conftest.py
@@ -2,14 +2,23 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture()
 def setup(request):
-    driver = webdriver.Chrome()
+    # Headless setup
+    options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+
+    driver = webdriver.Chrome(options=options)
     #driver.maximize_window()
-    driver.set_window_size(1920, 1080)
-    request.cls.driver = driver
-    yield
+    #driver.set_window_size(1920, 1080)
+    #request.cls.driver = driver
+    #yield
+    yield driver
     driver.quit()
 
 # ----------------- Capture Screenshots on Test Failure -----------------
