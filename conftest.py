efb603867e133c078cbf763188b369e89e61b3ca
CAST <dona_dea@hotmail.com>
2025-12-12 19:34:39 -0600

    Fix Chrome usage in Jenkins

--- conftest.py
+++ conftest.py
@@ -13,7 +13,7 @@ def setup():
     # 👇 THIS IS CRITICAL
     options.binary_location = "/usr/bin/google-chrome"
 
-    options.add_argument("--headless=new")
+    options.add_argument("--headless")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-gpu")
     options.add_argument("--disable-dev-shm-usage")
