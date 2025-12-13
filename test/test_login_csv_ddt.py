7cddc51b9441d48f34558477c0329724bfa0e0dc
CAST <dona_dea@hotmail.com>
2025-12-12 20:14:14 -0600

    Fix pytest fixture mismatch (driver)

--- test/test_login_csv_ddt.py
+++ test/test_login_csv_ddt.py
@@ -2,26 +2,23 @@ import pytest
 from pages.login_page import LoginPage
 from utils.read_csv import get_csv_data
 
+data = get_csv_data("testdata/login_data.csv")
 
-@pytest.mark.usefixtures("setup")
+
 class TestLoginCSVDDT:
 
-    data = get_csv_data("testdata/login_data.csv")
-
     @pytest.mark.parametrize("row", data)
-    def test_login_csv_ddt(self, row):
-        username = row[0]
-        password = row[1]
-        expected = row[2]
+    def test_login_csv_ddt(self, driver, row):
+        username = row["username"]
+        password = row["password"]
+        expected = row["expected"]
 
-        self.driver.get("https://the-internet.herokuapp.com/login")
-        login_page = LoginPage(self.driver)
+        driver.get("https://the-internet.herokuapp.com/login")
+        login_page = LoginPage(driver)
 
         login_page.enter_username(username)
         login_page.enter_password(password)
         login_page.click_login()
 
         flash = login_page.get_flash_message()
-
-        assert flash.is_displayed()
         assert expected in flash.text
